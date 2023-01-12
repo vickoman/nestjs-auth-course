ecd1032a786f138b9e6f956531e7e5d17516f4ca
vickoman <vicmandlagasca@gmail.com>
2023-01-12 00:08:03 -0500

    Add Guards public and privte routes

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,8 +1,11 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
+import { Auth } from './decorators/auth.decorator';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
+import { AuthType } from './enums/auth-type.enum';
 
+@Auth(AuthType.None)
 @Controller('authentication')
 export class AuthenticationController {
     constructor(private readonly authService: AuthenticationService) {}
@@ -17,7 +20,7 @@ export class AuthenticationController {
     async signIn(
         @Body() signInDto: SignInDto
     ) {
-       return this.authService.signIn(signInDto);
+        return this.authService.signIn(signInDto);
     }
 
 }
